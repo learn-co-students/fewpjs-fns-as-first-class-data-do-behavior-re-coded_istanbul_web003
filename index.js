5c8596a69547be74c30e1510aa5502e6360cb6b7
jennethydyrova <jennethydyrovaa@gmail.com>
2020-07-19 23:53:10 +0300

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,21 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(string) {
+
+  string = string.split(':')
+  if (parseInt(string) < 12 ) {
+    return "Good Morning";
+  } else if (parseInt(string) >= 12 && parseInt(string) <= 17) {
+    return "Good Afternoon";
+  } else {
+    return "Good Evening";
+  }
+
+}
 /* Write your implementation of displayMessage() */
+function displayMessage (string) {
+  let content = document.getElementById("greeting")
+  content.innerText = string;
+}
+console.log(greet('7:00'))
