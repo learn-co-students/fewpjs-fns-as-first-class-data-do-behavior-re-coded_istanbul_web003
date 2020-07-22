da305fcc79b26564c04c9fa83f577e269aecb445
Tarkan Mansuroğlu <tarkanmansuroglu@hotmail.com>
2020-07-22 21:16:18 +0300

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,19 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(string){
+  const hour = parseInt(string.slice(0,2));
+  if(hour< 12){
+    return "Good Morning"
+  }
+  else if (hour >=12 && hour < 17){
+    return "Good Afternoon"
+  }
+  else return "Good Evening"
+}
+
+function displayMessage(string2){
+    document.getElementById("greeting").innerText = string2;
+}
+
