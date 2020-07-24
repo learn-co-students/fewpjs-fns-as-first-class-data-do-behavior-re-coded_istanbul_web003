7a205b9e51993a7d4013326b705f9011774e802d
ihab-sensei <ib-mashhadani@hotmail.com>
2020-07-24 12:55:14 +0300

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,21 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+function greet(time) {
+  time = time.split(":", 2);
+  time = parseInt(time.join(""));
+  if (time < 1200) {
+    return "Good Morning";
+  } else if (1200 <= time && time < 1700) {
+    return "Good Afternoon";
+  } else if (time >= 1700 && time <= 2400) {
+    return "Good Evening";
+  } else if (time > 2400) {
+    return "Please insert a valid time";
+  }
+}
+
+function displayMessage(string) {
+  const element = document.querySelector("#greeting")
+  element.innerText = string
+}
