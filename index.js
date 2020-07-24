ba0948de6cb7de92e326a33fbf48e504b5aa89db
Hiba <hiba.mashfij@gmail.com>
2020-07-24 22:20:19 +0300

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,27 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeString) {
+  // timeString "12:00"
+  // timeString "15:00"
+  // Use the split function to get the hour part
+  let hourNumber = parseInt(timeString.split(':',1));
+ 
+  if (hourNumber < 12) {
+   return ('Good Morning'); 
+  }
+
+  else if (hourNumber >=12 &&  hourNumber <= 17) {
+    return ('Good Afternoon');
+  }
+
+  else if (hourNumber > 17) {
+    return ('Good Evening');
+  }
+}
+
+function displayMessage(messageToDisplay) {
+   let messageElement = document.getElementById('greeting');
+   messageElement.innerText = messageToDisplay;
+};
 /* Write your implementation of displayMessage() */
