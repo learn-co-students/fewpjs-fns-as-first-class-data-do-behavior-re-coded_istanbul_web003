bdb5aacbdb8f4b29d97f2e13bf7ff431afd84241
your_name <your_email@example.com>
2020-07-26 22:32:22 +0300

    Done.

--- index.js
+++ index.js
@@ -6,6 +6,23 @@ function handleClick(e) {
   const timeString = document.getElementById('time').value
   displayMessage(greet(timeString))
 }
+function greet(string) {
+  const splitted = string.split(':')
+  const num = parseInt(splitted)
+  if ( num < 12 ){
+    return 'Good Morning'
+    
+  } else if ( num >= 12 && num < 17) {
+
+  return 'Good Afternoon'
+  } else {
+    return 'Good Evening'
+  }
+}
+function displayMessage(string) {
+  let content = document.getElementById("greeting")
+  content.innerText = string
+}
 
 /* End Given Code, don't edit above here...*/
 
