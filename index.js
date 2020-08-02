b9835abed16a8780eecba8bfbabfc9f1b038759e
Majd Ajroudi <majd.ajroudi@gmail.com>
2020-08-02 20:41:49 +0300

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,22 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(string){
+  const t= parseInt(string.split(":")[0])
+  console.log(t)
+  if (t < 12){
+    return "Good Morning"
+  }
+  else if (12 <= t  && t <= 17){
+    return "Good Afternoon"
+  }
+  else{
+    return "Good Evening"
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(str){
+  console.log(str)
+  const greeting= document.querySelector("#greeting")
+  greeting.innerText= str
+}
