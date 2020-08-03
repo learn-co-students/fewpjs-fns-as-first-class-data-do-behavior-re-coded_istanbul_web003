4060be720808cf5b6eaabb14f7dc5845c0cf25f8
orcunsagirsoy <orcunsagirsoy@gmail.com>
2020-08-03 17:18:36 +0300

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,36 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(String){
+
+    const splittedTime = String.split(":");
+
+    const hour = splittedTime[0];
+
+    const time = parseInt(hour, 10);
+
+    if (time < 12)
+    {
+      return "Good Morning";
+    }
+    else if (time >= 12 && time < 17)
+    {
+      return "Good Afternoon";
+    }
+    else
+    {
+      return "Good Evening";
+    }
+
+}
+
+
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(String){
+    document.getElementById("greeting").innerText = String;
+    
+}
+
