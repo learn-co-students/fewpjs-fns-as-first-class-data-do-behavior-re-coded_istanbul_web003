2cdfa7b557b7901f8446fcb6dc90d3c2ede060f6
ibrahim aljarad <ibrahimealkalil@gmail.com>
2020-09-27 23:17:16 +0300

    Done.

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 
+function handleClick(e) {
+  const timeString = document.getElementById('time').value
+  displayMessage(greet(timeString))
+}
+/* End Given Code, don't edit above here...*/
 
+/* Write your implementation of greet() */
+function greet(time) {
+  const hourInString = time.split(":");
+  const hoursInInt = parseInt(hourInString,10);
 
-function greet(the_time){
-  let this_time = the_time.split(":");
-  let another_time = this_time[0].concat(this_time[1])
-    let result = parseInt(another_time,10);
-   
-    if (result <1200){
-      return "Good Morning"
-    } if ( (result >= 1200) && (reult <= 1700)){
-              return "Good Afternoon";
-            }
-        else if (result > 1700){
-          return "Good Evening"
-        
-        }
-        document.getElementById("greeting").innerHTML= result;
+  if (hoursInInt < 12) {
+    return displayMessage('Good Morning');
+  }
+  else if (hoursInInt >=12 && hoursInInt <= 17){
+    return displayMessage('Good Afternoon');
+  }
+  else {
+  return displayMessage('Good Evening');
+  }
 }
 
+
+/* Write your implementation of displayMessage() */
+function displayMessage(msg) {
+  const element = document.getElementById("greeting");
+  element.innerText = msg;
+  return msg;
+}
